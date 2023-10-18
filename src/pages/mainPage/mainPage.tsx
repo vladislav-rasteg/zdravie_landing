44912a5d933f9b4566b241ac91ef374925cf1fee
vladrast <rastegaev.design@gmail.com>
2023-10-18 18:50:12 +0500

    initial commit

---
+++ src/pages/mainPage/mainPage.tsx
@@ -0,0 +1,33 @@
+import {useRef, useState, useEffect} from "react"
+import firstHeroImage from '../../assets/firstHeroImage.png'
+import secondHeroImage from '../../assets/secondHeroImage.png'
+import thirdHeroImage from '../../assets/thirdHeroImage.png'
+import s from "./mainPage.module.css"
+
+const mainPage = () => {
+    return(
+        <div className={s.pageWrapper}>
+            <div className={s.rowFlex}>
+                <div className={s.columnFlex}>
+                    <div className={s.heroMainBlock}>
+                        <h1>Клиника <i>превентивной</i><br/>медицины для детей<br/>и взрослых</h1>
+                        <a href="https://yandex.ru/maps/-/CDafZKZ-" target="_blanc">Пермь, ул. Крупской, 67</a>
+                    </div>
+                    <div className={s.rowFlex}>
+                        <img className={s.firstHeroImage} src={firstHeroImage} />
+                        <img className={s.secondHeroImage} src={secondHeroImage} />
+                    </div>
+                    <div className={s.heroRecordBlock}>
+                        <h2>Записаться<br/>к специалисту</h2>
+                        <a className={s.whiteButton}>Онлайн запись</a>
+                    </div>
+                </div>
+                <div>
+                <img className={s.thirdHeroImage} src={thirdHeroImage} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default mainPage;

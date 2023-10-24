7b181c0e37d33391196b102816bdd38146743d91
Vladislav Rastegaev <99723680+vladislav-rasteg@users.noreply.github.com>
2023-10-24 13:16:20 +0500

    feat: services block

--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -4,6 +4,10 @@ import secondHeroImage from '../../assets/secondHeroImage.png'
 import thirdHeroImage from '../../assets/thirdHeroImage.png'
 import Navbar from "../../components/Navbar/navbar"
 import s from "./mainPage.module.css"
+import { ReactComponent as ChevronRight } from "../../assets/chevron_right.svg"
+import { ReactComponent as ChevronRightSmall } from "../../assets/chevron_right_small.svg"
+import { ReactComponent as ChevronLeftSmall } from "../../assets/chevron_left_small.svg"
+import TRimage from "../../assets/TR.png";
 
 const mainPage = () => {
     return(
@@ -36,7 +40,97 @@ const mainPage = () => {
                                             <br/>но и основные причины болезни</p>
                 <a className={s.aboutButton}>Записаться на прием</a>
             </div>
+
+            <div className={s.servicesBlock}>
+                <h1>Наши услуги</h1>
+                <div className={s.servicesGrid}>
+                    <div className={s.service1}>
+                        <h3>Массаж детский</h3>
+                        <div className={s.buttonRow}>
+                            <a className={s.button}>Подробнее</a>
+                        </div>
+                    </div>
+                    <div className={s.service2}>
+                        
+                    </div>
+                    <div className={s.service3}>
+                        <h3>Массаж женский</h3>
+                        <div className={s.buttonRow}>
+                            <a className={s.button}>Подробнее</a>
+                        </div>
+                    </div>
+                    <div className={s.service4}>
+                        <h3>Массаж мужской</h3>
+                        <div className={s.buttonRow}>
+                            <a className={s.button}>Подробнее</a>
+                        </div>
+                    </div>
+                    <div className={s.service5}>
+                        <p>Более 25 лет мы разрабатываем
+                        <br/>и применяем программы с самыми
+                        эффективными методами
+                        поддержания и восстановления
+                        здоровья для детей и взрослых</p>
+                    </div>
+                    <div className={s.service6}>
+                        <h3>Прием педиатра</h3>
+                        <div className={s.buttonRow}>
+                            <a className={s.button}>Подробнее</a>
+                        </div>
+                    </div>
+                    <div className={s.service7}>
+                        <h3>Наши курсы</h3>
+                        <a className={s.button}>Смотреть</a>
+                    </div>
+                    <div className={s.service8}>
+                        <h3>Биорезонансная <br/>терапия</h3>
+                        <div className={s.buttonRow}>
+                            <a className={s.button}>Подробнее</a>
+                        </div>
+                    </div>
+                    <div className={s.service9}>
+                        <h3>Кабинет здорового<br/>питания для детей<br/>и взрослых</h3>
+                        <div className={s.buttonRow}>
+                            <a className={s.button}>Подробнее</a>
+                        </div>
+                    </div>
+                    <div className={s.service10}>
+                        
+                    </div>
+                    <div className={s.service11}>
+                        <h3>Остеопатия</h3>
+                        <div className={s.buttonRow}>
+                            <a className={s.button}>Подробнее</a>
+                        </div>
+                    </div>
+                    <div className={s.service12}>
+                        <h3>Мануальная <br/>терапия</h3>
+                        <div className={s.buttonRow}>
+                            <a className={s.button}>Подробнее</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div className={s.ourDoctors}>
+                <div className={s.aboutDoctor}>
+                    <h1>Наши врачи</h1>
+                    <p>сообщения</p>
+                    <div className={s.ourDoctorsText}>
+                        <p>Все процедуры выполняются опытными<br />
+                        специалистами и применяются для<br />
+                        устранения причины болезни или профилактики.</p>
+                        <div className={s.nextButton}>
+                            <ChevronRight />
+                        </div>
+                    </div>
+                </div>
+                <div className={s.doctorImageWrapper}>
+                    <img className={s.doctorImage} src={TRimage} />
+                </div>
+            </div>
         </div>
+        
     )
 }
 

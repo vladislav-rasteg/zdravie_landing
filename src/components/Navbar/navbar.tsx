d764be8b614e32e06eb2e7d9695f1222dfc6176a
vladrast <rastegaev.design@gmail.com>
2023-10-30 13:19:08 +0500

    feat: programs block

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -2,9 +2,67 @@ import {useRef, useState, useEffect} from "react"
 import {ReactComponent as Logo} from '../../assets/logo.svg'
 import s from "./navbar.module.css"
 
-const navbar = () => {
+const Navbar = () => {
+
+    const [showNavbar, setShowNavbar] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    const useWindowDimensions = () => {
+
+    const hasWindow = typeof window !== 'undefined';
+    
+    function getWindowDimensions() {
+        const width = hasWindow ? window.innerWidth : null;
+        const height = hasWindow ? window.innerHeight : null;
+        return {
+        width,
+        height,
+        };
+    }
+    
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+    
+    useEffect(() => {
+        if (hasWindow) {
+        const handleResize = () => {
+            setWindowDimensions(getWindowDimensions());
+        }
+    
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+        }
+    }, [hasWindow]);
+        return windowDimensions;
+    }
+
+    const controlNavbar = () => {
+        if (typeof window !== "undefined") {
+            if (window.scrollY > lastScrollY) {
+            // if scroll down hide the navbar
+            setShowNavbar(false);
+            } else {
+            // if scroll up show the navbar
+            setShowNavbar(true);
+            }
+
+            // remember current page location to use in the next move
+            setLastScrollY(window.scrollY);
+        }
+    };
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+          window.addEventListener("scroll", controlNavbar);
+    
+          // cleanup function
+          return () => {
+            window.removeEventListener("scroll", controlNavbar);
+          };
+        }
+      }, [lastScrollY]);
+
     return(
-        <div className={s.navbarWrapper}>
+        <div className={`${s.navbarWrapper} ${!showNavbar && s.navbarHide}`}>
             <div className={s.navbar}>
                 <Logo />
                 <div className={s.navbarLinks}>
@@ -26,4 +84,4 @@ const navbar = () => {
     
 }
 
-export default navbar;
+export default Navbar;
--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -8,15 +8,69 @@ import { ReactComponent as ChevronRight } from "../../assets/chevron_right.svg"
 import { ReactComponent as ChevronRightSmall } from "../../assets/chevron_right_small.svg"
 import { ReactComponent as ChevronLeftSmall } from "../../assets/chevron_left_small.svg"
 import TRimage from "../../assets/TR.png";
+import ARimage from "../../assets/AR.png";
+import TSimage from "../../assets/TS.png";
+import programsContentChild from "../../assets/programsContentChild.png"
+import programsContentAdult from "../../assets/programsContentAdult.png"
+import { ReactComponent as FirstMessage } from "../../assets/firstMessage.svg"
+import { ReactComponent as SecondMessage } from "../../assets/secondMessage.svg"
+import { ReactComponent as SwitchRight } from "../../assets/switchRight.svg"
+import { ReactComponent as SwitchLeft } from "../../assets/switchLeft.svg"
 
-const mainPage = () => {
+const MainPage = () => {
+
+    const [doctorIndex, setDoctorIndex] = useState(1)
+
+    const [doctorsSwitcherClassName, setDoctorsSwitcherClassName] = useState(`${s.doctorImage}`)
+
+    const [programType, setProgramType] = useState(1)
+
+    const [selectedProgram, setSelectedProgram] = useState(1)
+
+    const switchDoctor = () => {
+        setDoctorsSwitcherClassName(`${s.doctorImage} ${s.doctorSwitched}`)
+        setTimeout(() => {
+            setDoctorsSwitcherClassName(`${s.doctorImage}`)
+        }, 1000)
+    }
+
+    const doctorForward = () => {
+        if(doctorIndex === 3){
+            setDoctorIndex(1)
+        } else {
+            setDoctorIndex(doctorIndex+1)
+        }
+        switchDoctor()
+    }
+
+    const doctorBackward = () => {
+        if(doctorIndex === 1){
+            setDoctorIndex(3)
+        } else {
+            setDoctorIndex(doctorIndex-1)
+        }
+        switchDoctor()
+    }
+
+    const switchProgramType = () => {
+        if(programType === 1){
+            setProgramType(2)
+        } else {
+            setProgramType(1)
+        }
+    }
+
+    const setSelectedProgramHandler = (index: number) => {
+        setSelectedProgram(index)
+    }
+
     return(
         <div className={s.pageWrapper}>
             <Navbar />
             <div className={s.rowFlex}>
                 <div className={s.columnFlex}>
                     <div className={s.heroMainBlock}>
-                        <h1>Клиника <i>превентивной</i><br/>медицины для детей<br/>и взрослых</h1>
+                        <h1>Центр <i>превентивной</i><br/>медицины для детей<br/>и взрослых</h1>
                         <a href="https://yandex.ru/maps/-/CDafZKZ-" target="_blanc">Пермь, ул. Крупской, 67</a>
                     </div>
                     <div className={s.rowFlex}>
@@ -115,25 +169,111 @@ const mainPage = () => {
             <div className={s.ourDoctors}>
                 <div className={s.aboutDoctor}>
                     <h1>Наши врачи</h1>
-                    <p>сообщения</p>
+                    <div className={s.messages}>
+                        <div className={s.firstMessage}>
+                            Кто в вашей команде?
+                            <FirstMessage className={s.firstMessageCorner} />
+                        </div>
+                        <div className={s.doctorMessageWrapper}>
+                            {
+                                doctorIndex === 1 ? 
+                                    <div className={s.doctorMessage}>
+                                        <p className={s.doctorName}>Татьяна Растегаева</p>
+                                        <p className={s.doctorMessageText}>
+                                        Врач педиатр, гомеопат, натуропат,<br/>
+                                        нутрициолог - диетолог, врач интегративной,<br/>
+                                        превентивной и антивозрастной медицины,<br/>
+                                        висцеральный терапевт, специалист<br/>
+                                        по массажу.</p>
+                                        <SecondMessage className={s.secondMessageCorner}/>
+                                    </div>
+                                :
+                                doctorIndex === 2 ?
+                                    <div className={s.doctorMessage}>
+                                        <p className={s.doctorName}>Алексей Растегаев</p>
+                                        <p className={s.doctorMessageText}>
+                                        Мануальный терапевт, специалист<br/>
+                                        по электронной биофункциональной<br/>
+                                        органометрии, биорезонансной терапии<br/>
+                                        и гомеосиниатрии. Специалист превентивной<br/>
+                                        и интегративной медецины.</p>
+                                        <SecondMessage className={s.secondMessageCorner}/>
+                                    </div>
+                                :
+                                    <div className={s.doctorMessage}>
+                                        <p className={s.doctorName}>Татьяна Сиротина</p>
+                                        <p className={s.doctorMessageText}>
+                                        Врач-натуропат, педиатр, висцеральный<br/>
+                                        терапевт.</p>
+                                        <SecondMessage className={s.secondMessageCorner}/>
+                                    </div>
+                            }
+                        </div>
+                    </div>
                     <div className={s.ourDoctorsText}>
                         <p>Все процедуры выполняются опытными<br />
                         специалистами и применяются для<br />
-                        устранения причины болезни или профилактики.</p>
-                        <div className={s.nextButton}>
+                        устранения причины болезни и профилактики.</p>
+                        <div onClick={doctorForward} className={s.nextButton}>
                             <ChevronRight  />
                         </div>
                     </div>
                 </div>
                 <div className={s.doctorImageWrapper}>
-                    <img className={s.doctorImage} src={TRimage} />
+                    <img className={doctorsSwitcherClassName} src={doctorIndex === 1 ? TRimage : doctorIndex === 2 ? ARimage : TSimage} />
                     <div className={s.switcher}>
-                        <div className={s.switcherButton}>
+                        <div onClick={doctorBackward} className={s.switcherButton}>
                             <ChevronLeftSmall />
                         </div>
-                        <div className={s.switcherButton}>
+                        <div onClick={doctorForward} className={s.switcherButton}>
                             <ChevronRightSmall />
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div className={s.programsSection}>
+                <div className={s.programsBlock}>
+                    <div className={s.programsHeading}>
+                        <h1>Программы для </h1>
+                        <h1 className={programType === 1 ? s.programTypeActive : s.programTypeDisabled} onClick={switchProgramType}>детей</h1>
+                        <h1> / </h1>
+                        <h1 className={programType === 2 ? s.programTypeActive : s.programTypeDisabled} onClick={switchProgramType}>взрослых</h1>
+                    </div>
+                    <div className={s.programsContentWrapper}>
+                        <img className={s.programsContentImage} src={programType === 1 ? programsContentChild : programsContentAdult} />
+                        <div className={s.clickToSwitch}>
+                            <SwitchRight />
+                            <p>Нажмите,<br/>
+                            чтобы выбрать<br/>
+                            программу</p>
+                            <SwitchLeft />
+                        </div>
+                        <div className={s.programsList}>
+                            <div className={s.programsPair}>
+                                <div className={selectedProgram === 1 ? `${s.program} ${s.activeProgram}` : s.program} onClick={() => {setSelectedProgramHandler(1)}}>
+                                    Здоровейка
+                                </div>
+                                <div className={selectedProgram === 2 ? `${s.program} ${s.activeProgram}` : s.program} onClick={() => {setSelectedProgramHandler(2)}}>
+                                    Говорун
+                                </div>
+                            </div>
+                            <div className={s.programsPair}>
+                                <div className={selectedProgram === 3 ? `${s.program} ${s.activeProgram}` : s.program} onClick={() => {setSelectedProgramHandler(3)}}>
+                                    Отличник
+                                </div>
+                                <div className={selectedProgram === 4 ? `${s.program} ${s.activeProgram}` : s.program} onClick={() => {setSelectedProgramHandler(4)}}>
+                                    Малышок
+                                </div>
+                            </div>
+                            <div className={selectedProgram === 5 ? `${s.program} ${s.activeProgram}` : s.program} onClick={() => {setSelectedProgramHandler(5)}}>
+                                Здоровый позвоночник
+                            </div>
+                        </div>
+                    </div>
+                    <div className={s.aboutProgram}>
+                    Здоровейка - комплексная программа для детей 
+                    с нарушениями осанки, сколиозом, частыми 
+                    головокружениями, болями, сниженным зрением.
                     </div>
                 </div>
             </div>
@@ -142,4 +282,4 @@ const mainPage = () => {
     )
 }
 
-export default mainPage;
+export default MainPage;
